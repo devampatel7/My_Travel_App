71215715aa4b73737bad7b5402f1d82cdd9bfc97
Devam Patel <devampatel7@gmail.com>
2022-08-28 16:59:55 -0400

    Updated navbar to navigate through homepage and search pages

--- src/views/Navbar.js
+++ src/views/Navbar.js
@@ -6,9 +6,9 @@ const Navbar = () => {
     return(
         <div className="head">
             <nav>
-                <a href="" className="logo">Home</a>
+                <a href="/" className="logo">Home</a>
                 <a href="">About Us</a>
-                <a href="">Services</a>
+                <a href="/Search">Search</a>
                 <a href="">Contact</a>
                 <a href="" id="login">Login</a>
             </nav>
